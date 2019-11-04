59a1a33544d3ba0b574013ee23f2208c417c7c28
yonaper1 <57345110+yonaper1@users.noreply.github.com>
2019-11-04 20:41:40 +0200

    Update discord.lua

--- discord.lua
+++ discord.lua
@@ -4,14 +4,14 @@ Citizen.CreateThread(function()
         
         Citizen.Wait(5*1000) -- checks every 5 seconds (to limit resource usage)
         
-        SetDiscordAppId(640944828502245376) -- client id (int)
+        SetDiscordAppId(Change it to your id) -- client id (int)
 
         SetRichPresence(GetPlayerName(PlayerId()) .. " is on " .. GetStreetNameFromHashKey(GetStreetNameAtCoord(table.unpack(GetEntityCoords(player))) )) -- main text (string)
 
-        SetDiscordRichPresenceAsset("big") -- large logo key (string)
-        SetDiscordRichPresenceAssetText("Players:  " ..GetNumberOfPlayers().. "/64 ") -- Large logo "hover" text (string)
+        SetDiscordRichPresenceAsset("Change to your photo name This will be the photo of the server") -- large logo key (string)
+        SetDiscordRichPresenceAssetText("Players:  " ..GetNumberOfPlayers().. "/32 ") -- Large logo "hover" text (string)
 
-        SetDiscordRichPresenceAssetSmall("health") -- small logo key (string)
+        SetDiscordRichPresenceAssetSmall("Change to your photo name This will be the photo of the Health") -- small logo key (string)
         SetDiscordRichPresenceAssetSmallText(GetPlayerName(PlayerId()) .. " Health Is: "..(GetEntityHealth(player) -100).. "%") -- small logo "hover" text (string)
     end
 end)
